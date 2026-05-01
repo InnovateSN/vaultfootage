f0b32f9357c705a7a4e65399f34d435123b3a576
InnovateSN <innovatesolutionsnow@gmail.com>
2026-05-01 13:05:41 -0700

    Use hardware image for TamaSocial landing

--- app/page.js
+++ app/page.js
@@ -6,37 +6,25 @@ export default function Home() {
         width: "100vw",
         margin: 0,
         overflow: "hidden",
-        background:
-          "radial-gradient(circle at center, #171923 0%, #090b12 58%, #04050a 100%)",
+        background: "#000",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
         position: "relative",
       }}
     >
-      <div
+      <img
+        src="/assets/tamasocial-hardware/tamasocial-hardware-landing.jpg?v=1"
+        alt="TamaSocial virtual pet hardware"
         style={{
-          position: "relative",
-          width: "min(94vw, 1180px)",
-          aspectRatio: "16 / 10",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          width: "100vw",
+          height: "100vh",
+          objectFit: "contain",
+          display: "block",
+          userSelect: "none",
+          pointerEvents: "none",
         }}
-      >
-        <img
-          src="/assets/tamasocial-separated/01-shell-main.png?v=3"
-          alt=""
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "contain",
-            display: "block",
-            userSelect: "none",
-            pointerEvents: "none",
-          }}
-        />
-      </div>
+      />
     </main>
   );
 }
